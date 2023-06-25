db82a3acac68e0bde78f19d693c629136bfe032c
Zhadyra <t_zhadyra_t@mail.ru>
2023-06-25 18:49:16 +0600

    lala

--- client/src/components/Hero.jsx
+++ client/src/components/Hero.jsx
@@ -13,17 +13,17 @@ const Hero = () => {
   </p>*/}
         </div>
 
-        <div className="flex flex-row justify-between items-center w-full">
-          <h1 className="flex-1 font-poppins font-semibold ss:text-[55px] text-[40px] text-white ss:leading-[100.8px] leading-[75px]">
-          Посетите серию <br className="sm:block hidden" />{" "}
-            <span className="text-gradient">моих вебинаров</span>{" "} и узнайте ответы
+        <div >
+          <h1 className="flex-1 font-poppins font-semibold ss:text-[45px] text-[40px] text-white ss:leading-[100.8px] leading-[75px]">
+          Откройте безлимитное количество<br className="sm:block hidden" />{" "}
+            <span className="text-gradient">знаний</span>{" "} посетив вебинары, и узнайте ответы
           </h1>
           <div className="ss:flex hidden md:mr-4 mr-0">
-            <GetStarted />
           </div>
         </div>
+        
 
-        <h1 className="font-poppins font-semibold ss:text-[55px] text-[40px] text-white ss:leading-[100.8px] leading-[75px] w-full">
+        <h1 className="font-poppins font-semibold ss:text-[45px] text-[40px] text-white ss:leading-[100.8px] leading-[75px] w-full">
         на эти вопросы:
         </h1>
         <p className={`${styles.paragraph} max-w-[660px] mt-4`}>
@@ -32,11 +32,10 @@ const Hero = () => {
         Уделяя при этом не более 1 часа в неделю?
         </p>
       </div>
-
       <div className={`flex-1 flex ${styles.flexCenter} md:my-0 my-10 relative`}>
-        <img src={olzhas} alt="billing" className="w-[100%] h-[100%] relative z-[0] rounded-full" />
+        <img src={olzhas} alt="billing" className="w-[100%] h-[100%]  rounded-full" />
 
-        {/* gradient start */}
+        {/* gradient start 
         <div className="absolute z-[0] w-[40%] h-[35%] top-0 pink__gradient" />
         <div className="absolute z-[1] w-[80%] h-[80%] rounded-full white__gradient bottom-40" />
         <div className="absolute z-[0] w-[50%] h-[50%] right-20 bottom-20 blue__gradient" />
